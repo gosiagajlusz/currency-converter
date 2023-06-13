5c59598ece6430ea123e2f45534b7b6ac1c5b56a
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2023-06-13 18:57:04 +0200

    turn rate into a varieble

--- js/script.js
+++ js/script.js
@@ -2,12 +2,13 @@ let euroInput = document.querySelector(".euro");
 let rateInput = document.querySelector(".rate");
 let formElement = document.querySelector(".js-form");
 let valueInPlnElement = document.querySelector(".js-valueInPln");
+let rate = 4.48
 
 formElement.addEventListener("submit", (event) => {
 event.preventDefault();
 
 let euro = euroInput.value;
-let rate = rateInput.value;
+//let rate = rateInput.value;
 let value = (euro*rate);
 valueInPlnElement.innerText = value.toFixed(3);
 });
