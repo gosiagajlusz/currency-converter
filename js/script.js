5e48936cd6186f1e840e81e235ad7180fde9791e
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2023-06-14 17:26:26 +0200

    final cleaning

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 let euroInput = document.querySelector(".form__euro");
 let formElement = document.querySelector(".form");
 let valueInPlnElement = document.querySelector(".js-valueInPln");
-let rate = 4.48
+let rateInput = document.querySelector(".form__rate")
 
 formElement.addEventListener("submit", (event) => {
     event.preventDefault();
 
     let euro = euroInput.value;
+    let rate = rateInput.value;
 
     let value = (euro * rate);
     valueInPlnElement.innerText = value.toFixed(3);
