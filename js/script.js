36d5725c66b570c4ed834db0403e65f97511f9a9
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2023-07-01 15:44:55 +0200

    comments with  what was wrong-for future

--- js/script.js
+++ js/script.js
@@ -6,31 +6,68 @@ let formElement = document.querySelector(".js-form");
 //formularz cały
 let valueInPlnElement = document.querySelector(".js-valueInPln");
 //kwota w złotówkach do wyświetlenia
-let euroRate = 4.48
-let dolarRate = 4.08
-let frankSzwajcarskiRate = 4.57
 let money = moneyInput.value;
 let rate = currencySelect.value;
+// let euroRate = 4.48
+// let dolarRate = 4.08
+// let frankSzwajcarskiRate = 4.57
+
+
+
+// formElement.addEventListener("submit", (event) => {
+//   event.preventDefault();
+
+//   switch (rate) {
+
+
+//     case "euro":
+//       valueInPlnElement = money * euroRate;
+//       break;
+
+//     case "dolar":
+//       valueInPlnElement = money * dolarRate;
+//       break;
+
+//     case "frank szwajcarski":
+//       valueInPlnElement = money * frankSzwajcarskiRate;
+//       break;
+
+
+
+//   }
+//   valueInPlnElement.textContent = valueInPlnElement.toFixed(2);
+
+// });
+
+
+let euroRate = 4.48;
+let dolarRate = 4.08;
+let frankSzwajcarskiRate = 4.57;
 
 formElement.addEventListener("submit", (event) => {
   event.preventDefault();
-  
+  let money = +moneyInput.value;
+  let rate = currencySelect.value;
+  console.log(money, rate);
+  let result;
+  //result to zmienna w ktorą się pakuje ten wynik
+
   switch (rate) {
     case "euro":
-      valueInPlnElement = money * euroRate;
+      result = money * euroRate;
       break;
 
     case "dolar":
-      valueInPlnElement = money * dolarRate;
+      result = money * dolarRate;
       break;
 
     case "frank szwajcarski":
-      valueInPlnElement = money * frankSzwajcarskiRate;
+      result = money * frankSzwajcarskiRate;
       break;
-
-
-
   }
-  valueInPlnElement.innerText = ${valueInPlnElement.toFixed(2)};
-
+  valueInPlnElement.innerHTML = result.toFixed(2);
+  //result wskakuje do wewnętrznego tekstu pola valueInPln
 });
+
+//podsumowując: traktowalam jak zmienną coś,co nią nie jest
+//bo to jest napis, a nie zmienna xD
